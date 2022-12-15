8d895e5562f3cb825a975778e1802abd73b2ca6c
y-jun1109 <hsol10242@naver.com>
2022-12-15 12:44:28 +0900

    2022-12-15 [juni] 신고하기-로그인한 회원별로 글작성 추가

--- GDJ56_wdh_semi/src/main/java/com/wdh/del/controller/FileDownServlet.java
+++ GDJ56_wdh_semi/src/main/java/com/wdh/del/controller/FileDownServlet.java
@@ -33,14 +33,15 @@ public class FileDownServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		//클라이언트가 보낸 파일명을 가져옴
 		String fileName=request.getParameter("filename");
-		
-		String path=getServletContext().getRealPath("/upload/notice/");
+		//지정한 저장경로에서 보낸 파일명과 일치하는 파일을 가져옴
+		//InputStream 사용
+		String path=getServletContext().getRealPath("/upload/cs/");
 		FileInputStream fis=new FileInputStream(path+fileName);
 		BufferedInputStream bif=new BufferedInputStream(fis);
 		
-		
+//		클라이언트에게 보낼 파일명을 인코딩 처리하기
 		String fileReName="";
 		String header=request.getHeader("user-agent");
 		boolean isMSIE=header.indexOf("MSIE")!=-1||header.indexOf("Trident")!=-1;
@@ -50,15 +51,16 @@ public class FileDownServlet extends HttpServlet {
 			fileReName=new String(fileName.getBytes("utf-8"),"ISO-8859-1");
 		}
 		
-	
+		//응답 헤더 설정
+		//content-type에 대한 설정
 		response.setContentType("application/octet-stream");
 		response.setHeader("Content-disposition", "attachment;filename="+fileReName);
 		
-		
+		//클라이언트 연결 스트림 열기
 		ServletOutputStream sos=response.getOutputStream();
 		BufferedOutputStream bos=new BufferedOutputStream(sos);
 		
-	
+		//연결된 스트림으로 파일 전송하기
 		int read=-1;
 		while((read=bif.read())!=-1) {
 			bos.write(read);
