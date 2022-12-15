b74314e4b0bd458382212fa0b9f219f1d01e9159
GDJ56 <GDJ56@DESKTOP-HE2L8DR>
2022-12-15 16:20:09 +0900

    mypage 후기글 목록까지, 삭제버튼기능 수정 중

---
+++ GDJ56_wdh_semi/src/main/java/com/wdh/mypage/controller/DeleteReviewServlet.java
@@ -0,0 +1,65 @@
+package com.wdh.mypage.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wdh.board.vo.ReviewBoard;
+import com.wdh.mypage.service.MypageService;
+
+/**
+ * Servlet implementation class DeleteReviewServlet
+ */
+@WebServlet("/mypage/deleteReview.do")
+public class DeleteReviewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteReviewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+		
+		int result = new MypageService().deleteReview(boardNo);
+		
+		String msg="", loc="";
+		
+		if(result>0) {
+			
+			msg="삭제 성공!";
+			loc="/mypage/mycontent.do";
+			
+		}else {
+			
+			msg="삭제 실패!";
+			loc="/mypage/mycontent.do";
+			
+		}
+		
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		request.getRequestDispatcher("/views/common/msgm.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
