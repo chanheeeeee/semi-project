52104bd044cdb2ec94f211c1859ebd8598d98c15
chanhee <chanhee@DESKTOP-PS8QVL3>
2022-12-16 12:29:58 +0900

    [chan]동행 참가신청, 참가취소

---
+++ GDJ56_wdh_semi/src/main/java/com/wdh/board/controller/WdJoinServlet.java
@@ -0,0 +1,62 @@
+package com.wdh.board.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wdh.board.service.BoardService1;
+import com.wdh.board.vo.WdJoin;
+import com.wdh.member.service.MemberService;
+import com.wdh.member.vo.Member;
+
+/**
+ * Servlet implementation class WdJoinServlet
+ */
+@WebServlet("/board/wdjoin.do")
+public class WdJoinServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WdJoinServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int memberNo=Integer.parseInt(request.getParameter("memberNo"));
+		int wdNo=Integer.parseInt(request.getParameter("wdNo"));
+		System.out.println(memberNo+wdNo);
+		WdJoin wj=WdJoin.builder().memberNo(memberNo).wdNo(wdNo).build();
+		System.out.println(wj);
+		int result=new BoardService1().wdJoin(wj);
+		String msg="", loc="";
+		if(result>0) {
+			msg="참가신청 완료!";
+			loc="/board/boardView.do?boardNo=" + wdNo;
+		}else {
+			msg="참가신청 실패!";
+			loc="/board/boardView.do?boardNo=" + wdNo;
+		}
+		request.setAttribute("msg", msg);
+		request.setAttribute("loc", loc);
+		request.getRequestDispatcher("/views/common/msgch.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
